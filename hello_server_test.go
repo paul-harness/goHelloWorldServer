64112b316f7f43376b0cfc90240916cb331f0920
paul-harness <56152762+paul-harness@users.noreply.github.com>
2021-01-27 04:05:08 -0500

    Update hello_server_test.go

--- hello_server_test.go
+++ hello_server_test.go
@@ -16,92 +16,18 @@ func TestGreetingSpecificDemo(t *testing.T) {
 	}
 }
 
+func TestShowFailure(t *testing.T) {
+	greeting := CreateGreeting("Demo1")
+	if greeting != "Hello, Demo\n" {
+		t.Errorf("Intentional failure. got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
+
+
 func TestGreetingDefault(t *testing.T) {
 	greeting := CreateGreeting("")
 	if greeting != "Hello, Guest\n" {
 		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
 	}
 }
-
-func TestGreetingPaul1(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul2(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingePaul3fault(t *testing.T) {
-	greeting := CreateGreDeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul4(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul5(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul6(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul7(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul8(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul9(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-func TestGreetingPaul10(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-
-
-
-func TestGreetingPaul11(t *testing.T) {
-	greeting := CreateGreeting("")
-	if greeting != "Hello, Guest\n" {
-		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Guest\n")
-	}
-}
-
-
-
