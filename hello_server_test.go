4a9999ce39fa51fda2da02b51ed114be3c993606
paul-harness <56152762+paul-harness@users.noreply.github.com>
2021-01-27 04:22:44 -0500

    Update hello_server_test.go

--- hello_server_test.go
+++ hello_server_test.go
@@ -23,6 +23,48 @@ func TestGreetingSpecificPaul(t *testing.T) {
 	}
 }
 
+func TestGreetingSpecificPaul2(t *testing.T) {
+	greeting := CreateGreeting("Paul2")
+	if greeting != "Hello, Paul2\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
+func TestGreetingSpecificPaul3(t *testing.T) {
+	greeting := CreateGreeting("Paul3")
+	if greeting != "Hello, Paul3\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
+func TestGreetingSpecificPaul4(t *testing.T) {
+	greeting := CreateGreeting("Paul4")
+	if greeting != "Hello, Paul4\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
+func TestGreetingSpecificPaul5(t *testing.T) {
+	greeting := CreateGreeting("Paul5")
+	if greeting != "Hello, Paul5\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
+func TestGreetingSpecificPaul6(t *testing.T) {
+	greeting := CreateGreeting("Paul6")
+	if greeting != "Hello, Paul6\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
+func TestGreetingSpecificPaul7(t *testing.T) {
+	greeting := CreateGreeting("Paul7")
+	if greeting != "Hello, Paul7\n" {
+		t.Errorf("Greeting was incorrect, got: %s, want: %s.", greeting, "Hello, Demo\n")
+	}
+}
+
 func TestShowFailure(t *testing.T) {
 	greeting := CreateGreeting("Demo1")
 	if greeting != "Hello, Demo\n" {
